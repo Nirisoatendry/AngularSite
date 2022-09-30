66e100e5e6d29f58c96094bd4e5394f034e8bc11
NirinaMickael <todisoanirinamickael@gamil.com>
2022-09-30 14:41:37 +0300

    features-accueil

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from 'src/app/features/accueil/accueil.component';
 import { ContactComponent } from 'src/app/features/contact/contact.component';
 import { DashboardComponent } from './dashboard.component';
 
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path:'contact',
     component:ContactComponent
+  },
+  {
+    path:'',
+    component:AccueilComponent
   }
 ];
 
